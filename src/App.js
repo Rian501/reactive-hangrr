b919c34e4115cb1f918555268378e3ee7502d1e3
Emily Lemmon <eclaire.lemmon@gmail.com>
2019-02-05 20:18:45 -0600

    add in firebase, start to install guest check

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from "./Navbar";
+import hiddenKeys from "./values"
+import ApplicationViews from "./ApplicationViews";
 
-class App extends Component {
+const config = {
+  apiKey: hiddenKeys.FirebaseKey,
+  authDomain: hiddenKeys.FirebaseAuthDomain
+}
+
+firebase.initializeApp(config);
+var provider = new firebase.auth.GoogleAuthProvider();
+export default class App extends Component {
+  state = {
+    currentUser: "guest"
+  }
+
+  setCurrentUser(uid) {
+    this.setState({ currentUser: uid })
+  }
+
+  componentDidMount() {
+
+  }
+
+
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <Router>
+        <React.Fragment>
+          <Navbar />
+        </React.Fragment>
+      </Router>
     );
   }
 }
-
-export default App;
---
+++ src/ApplicationViews.js
@@ -0,0 +1,26 @@
+import { Route, Redirect } from "react-router-dom";
+import React, { Component } from "react";
+
+export default class ApplicationViews extends Component {
+  state = {
+    
+  };
+
+  // Check if there is a logged-in user
+
+  
+
+  componentDidMount() {
+    // set the state with the props?
+    };
+
+    
+  //ref this: https://github.com/nss-day-cohort-29/Kennel-React-29/blob/master/src/components/ApplicationViews.js
+  render() {
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+  }
+}
